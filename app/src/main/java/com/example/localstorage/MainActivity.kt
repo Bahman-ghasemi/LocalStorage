f4b0597198d9d4646b0e9bb0d4654286d93e87c1
Bahman <ghasemi.bahman2014@gmail.com>
2024-06-30 10:38:08 +0330

    Shared Preferences

--- app/src/main/java/com/example/localstorage/MainActivity.kt
+++ app/src/main/java/com/example/localstorage/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.localstorage
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,12 +9,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.localstorage.ui.theme.LocalStorageTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,9 +39,33 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize()
                             .padding(innerPadding),
                         horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
+                        verticalArrangement = Arrangement.spacedBy(
+                            12.dp,
+                            alignment = Alignment.CenterVertically
+                        )
                     ) {
+                        val prefs = getSharedPreferences("Prefs", Context.MODE_PRIVATE)
+                        var name by remember { mutableStateOf("") }
+                        var age by remember { mutableStateOf("") }
 
+                        OutlinedTextField(value = name, onValueChange = { name = it }, label = { Text(text = "Name")})
+                        OutlinedTextField(value = age, onValueChange = { age = it }, label = { Text(text = "Age")})
+                        Button(onClick = {
+                            prefs.edit()
+                                .putString("name_key", name)
+                                .putInt("age_key", age.toIntOrNull() ?: -1)
+                                .apply()
+                        }) {
+                            Text(text = "Save")
+                        }
+
+                        OutlinedButton(onClick = {
+                            name = prefs.getString("name_key", "") ?: ""
+                            age = prefs.getInt("age_key", -1).toString()
+
+                        }) {
+                            Text(text = "Load")
+                        }
                     }
                 }
             }
